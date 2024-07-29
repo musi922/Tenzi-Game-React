d5bfea3cc68549ad0ee0a868e63f2be1d03e55b1
Richard <richardmusime6@gmail.com>
2024-07-29 12:20:35 +0200

    Adding styles to the dies face

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './App.css'
+import Die from './components/Die'
 
 function App() {
 
   return (
-    <main></main>
+    <main>
+     <div className="dice-container">
+                <Die value="1" />
+                <Die value="2" />
+                <Die value="3" />
+                <Die value="4" />
+                <Die value="5" />
+                <Die value="6" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+            </div>
+    </main>
   )
 }
 
